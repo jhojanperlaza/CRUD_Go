09352eb539a7936d33464cee40205efaa278a99f
jhojan <jhojan_perlaza123>
2022-12-16 10:38:38 -0500

    [ADD] dir with first steps in GO

---
+++ hello_go/hello_web.go
@@ -0,0 +1,21 @@
+package main
+
+import "net/http"
+
+func main() {
+
+	// routes
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
+
+	// start http server
+	http.ListenAndServe(":3000", nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) { 
+	w.Write([]byte("Hello web"))
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) { 
+	w.Write([]byte("Contact pages"))
+}
